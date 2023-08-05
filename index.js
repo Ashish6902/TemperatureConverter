254ba960fd18a06bb805714f31ea7d65956b2151
Ashish <99881967+Ashish6902@users.noreply.github.com>
2023-08-05 14:43:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
- document.getElementById('convert').onclick = tempConvert;
-document.getElementById('clear').onclick = clearForm;
+document.addEventListener("DOMContentLoaded", function () {
+  //to get values use anonymous function
+    var convertButton = document.getElementById("Convert");
+    var ResetButton = document.querySelector("input[type='reset']");
+    var outputElement = document.getElementById("output");
+    var formulaElement = document.getElementById("Formula");
+// add event listner to to button
+    convertButton.addEventListener("click", function (event) {
+        event.preventDefault();
 
-function tempConvert() {
-    
-    var fahrenheit = document.getElementById("fahrenheit").value;
-    var celsius = document.getElementById("celsius").value;
-  
-  if (fahrenheit != '') {
-        celsius = (parseFloat(fahrenheit) - 32) / 1.8;
-    } else {
-        fahrenheit = (parseFloat(celsius) * 1.8) + 32;
-    }
-  
+        var input_Value = parseFloat(document.getElementById("input_area").value);
+        var selectedUnit = document.querySelector("select").value;
+        var converted_Value;
+        var formula;
+            //use if else to check what value is selected 
+        if (selectedUnit === "Fahrenheit") {
+            converted_Value = (input_Value * 9/5) + 32;
+            formula = `${input_Value}°C × 9/5 + 32 = ${converted_Value}°F`;
+        } else if (selectedUnit === "Kelvin") {
+            converted_Value = input_Value + 273.15;
+            formula = `${input_Value}°C + 273.15 = ${converted_Value}K`;
+        }
+            // to change inner html
+        outputElement.textContent = `Converted Temperature: ${converted_Value} ${selectedUnit}`;
+        formulaElement.textContent = `Formula: ${formula}`;
+    });
 
-
-    document.getElementById('fahrenheit').value = parseFloat(fahrenheit).toFixed(1);
-    document.getElementById('celsius').value = parseFloat(celsius).toFixed(1);
-}
-
-
-function clearForm() {
-    document.getElementById('fahrenheit').value = '';
-    document.getElementById('celsius').value = '';
-} 
-
+    //to reset everything
+    ResetButton.addEventListener("click", function () {
+        outputElement.textContent = "";
+        formulaElement.textContent = "";
+    });
+});
